0f6e1c81df7135980ea7228b57e61e813b6c6aa3
Suman Mukherjee <sumanmukherjee03@gmail.com>
2018-05-04 19:18:38 -0700

    Some minor improvements in the build script

--- build.sh
+++ build.sh
@@ -3,7 +3,6 @@
 set -e
 
 ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-REGISTRY_OWNER=sumanmukherjee03
 
 trap "cd $ROOT_DIR" TERM
 
@@ -12,14 +11,28 @@ err() {
   exit 1
 }
 
-main() {
+validate() {
   local image="$1"
   local version="$2"
+  [[ ! -z $DOCKERHUB_USERNAME ]] \
+    || err "Please have \$DOCKERHUB_USERNAME exported in your shell"
+  [[ ! -z $DOCKERHUB_PASSWORD ]] \
+    || err "Please have \$DOCKERHUB_PASSWORD exported in your shell cause you cant push the images otherwise"
+  [[ -f "$HOME/.docker/config.json" ]] \
+    || err "You dont have docker configured on this user"
+  test ! -z "$(cat "$HOME/.docker/config.json" | jq -r '.auths | .[] | .auth')" \
+    || err "You are not logged into dockerhub"
   [[ -d "$ROOT_DIR/$image/$version" ]] \
     || err "The image and version provided doesnt exist"
+}
+
+main() {
+  validate "$@"
+  local image="$1"
+  local version="$2"
   pushd .
   cd "$ROOT_DIR/$image/$version"
-  docker build -t $REGISTRY_OWNER/$image:$version .
+  docker build -t $DOCKERHUB_USERNAME/$image:$version .
   popd
 }
 
