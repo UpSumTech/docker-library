b2872e33c5e0bb19ccfdae31df2c2a7409ec90d3
Suman Mukherjee <sumanmukherjee03@gmail.com>
2018-05-13 00:25:58 -0700

    Added steps for cutting a release from a repo

--- docker-builder/1.13.1/entrypoint.sh
+++ docker-builder/1.13.1/entrypoint.sh
@@ -4,8 +4,31 @@
 
 DOCKER_SOCKET="/var/run/docker.sock"
 DOCKER_CONFIG="$HOME/.docker/config.json"
+PROJECT_TMPDIR=$(mktemp -d "$TMPDIR/build.XXXX")
+
+trap "rm -rf $PROJECT_TMPDIR; echo 'Cleaned up tmp dir'" EXIT
+
+validate_env_vars() {
+  [[ ! -z "$GITHUB_USERNAME" ]] \
+    || die "GITHUB_USERNAME is not exported"
+  [[ ! -z "$DEPLOY_GITHUB_TOKEN" ]] \
+    || die "GITHUB_TOKEN is not exported"
+  [[ ! -z "$DOCKER_USERNAME" ]] \
+    || die "DOCKER_USERNAME is not exported"
+  [[ ! -z "$BINTRAY_USERNAME" ]] \
+    || die "BINTRAY_USERNAME is not exported"
+  [[ ! -z "$BINTRAY_REPO_NAME" ]] \
+    || die "BINTRAY_REPO_NAME is not exported"
+  [[ ! -z "$BINTRAY_API_KEY" ]] \
+    || die "BINTRAY_REPO_NAME is not exported"
+  [[ ! -z "$GIT_REPO_NAME" ]] \
+    || die "REPO_NAME is not exported"
+}
 
 validate() {
+  local git_repo_url="$1"
+  local git_branch="$2"
+  local version="$3"
   [[ "$(id -u)" == "0" ]] \
     || die "container is not running as root"
   command -v docker \
@@ -18,13 +41,68 @@ validate() {
     || die "docker config not present"
   [[ ! -z "$(cat "$DOCKER_CONFIG" | jq -r ".auths | .[] | .auth")" ]] \
     || die "not logged into docker registry"
+  [[ "$git_repo_url" =~ "https" ]] \
+    || die "only accepts git urls with https over token auth for release"
+  [[ "$git_repo_url" =~ (master|develop|wip|hotfix) ]] \
+    || die "only accepts git branches (master|develop|wip|hotfix) for release"
+  [[ "$version" =~ (major|minor|patch) ]] \
+    || die "only accepts release versions (major|minor|patch) for release"
+  validate_env_vars
+  ok
+}
+
+get_repo_name() {
+  local repo_url="$1"
+  local repo_name="$(echo $repo_url | cut -d '/' -f5 | sed -e 's#.git##g')"
+  echo $repo_name
+}
+
+clone_repo() {
+  local git_repo_url="$1"
+  local git_branch="$2"
+  local repo_url="$(echo "$git_repo_url" | sed -e "s#github.com#$DEPLOY_GITHUB_TOKEN@github.com#g")"
+  cd "$PROJECT_TMPDIR"
+  git clone "$repo_url"
+  cd "$(get_repo_name "$repo_url")"
+  git fetch --all
+  git checkout "$git_branch"
+  git status
+  ok
+}
+
+release() {
+  local git_repo_url="$1"
+  local git_branch="$2"
+  local version="$3"
+  cd "$PROJECT_TMPDIR/$(get_repo_name "$repo_url")"
+  case "$git_branch" in
+    *wip*)
+      make release WIP=1 VERSION="$version" \
+        || die "could not release from branch $git_branch for version level $version"
+      ;;
+    *hotfix*)
+      make release HOTFIX=1 VERSION="$version" \
+        || die "could not release from branch $git_branch for version level $version"
+      ;;
+    master|develop)
+      make release HOTFIX=1 VERSION="$version" \
+        || die "could not release from branch $git_branch for version level $version"
+      ;;
+    *)
+      die "Dont know how to build and release from branch $git_branch"
+      ;;
+  esac
   ok
 }
 
 main() {
+  local git_repo_url="$1"
+  local git_branch="$2"
+  local version="$3"
   log_current_state
-  validate
-  docker images
+  validate "$git_repo_url" "$git_branch" "$version"
+  chdir_and_exec clone_repo "$git_repo_url" "$git_branch"
+  chdir_and_exec release "$git_repo_url" "$git_branch" "$version"
 }
 
 [[ $BASH_SOURCE == $0 ]] && main "$@"
