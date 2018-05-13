410a8bcd77b94f0ca53b578c92240fc4be532bcb
Suman Mukherjee <sumanmukherjee03@gmail.com>
2018-05-12 19:37:41 -0700

    Added some more validations for the docker image builder

--- docker-builder/1.13.1/entrypoint.sh
+++ docker-builder/1.13.1/entrypoint.sh
@@ -3,14 +3,21 @@
 [[ -f "/usr/local/share/bash_utils.sh" && ! $BASH_UTILS_SOURCED -eq 1 ]] && . "/usr/local/share/bash_utils.sh"
 
 DOCKER_SOCKET="/var/run/docker.sock"
+DOCKER_CONFIG="$HOME/.docker/config.json"
 
 validate() {
+  [[ "$(id -u)" == "0" ]] \
+    || die "container is not running as root"
   command -v docker \
     || die "docker is not installed"
   [[ -e $DOCKER_SOCKET ]] \
     || die "docker socket is not present"
   curl -ivs --unix-socket $DOCKER_SOCKET http://localhost/version \
     || die "cant communicate with the docker socket"
+  [[ -f "$DOCKER_CONFIG" ]] \
+    || die "docker config not present"
+  [[ ! -z "$(cat "$DOCKER_CONFIG" | jq -r ".auths | .[] | .auth")" ]] \
+    || die "not logged into docker registry"
   ok
 }
 
